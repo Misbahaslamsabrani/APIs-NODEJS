65b2c437e42954b44dd9176607d0db27d5846e09
misbahaslamsabrani <misbahaslamsabrani@gmail.com>
2019-04-15 04:37:34 +0500

    done!

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ if (city) {
     getData(city);
 }
 else {
-    var ipgeolocationApi = new IPGeolocationAPI("85c4e3caca5c4d8abcd4bca8173284d7", false); 
+    var ipgeolocationApi = new IPGeolocationAPI("85c4e3caca5c4d8abcd4bca8173284d7", false);
     ipgeolocationApi.getGeolocation((res) => {
-            if(res){
-                getData(res.city)
-            }
-            else{
-                console.log(chalk.bold.red("Sorry, something went wrong!"))
-            }
+        if (res) {
+            getData(res.city)
+        }
+        else {
+            console.log(chalk.bold.red("Sorry, something went wrong!"))
+        }
     });
 }
 
@@ -26,59 +26,68 @@ function getData(city) {
         json: true,
     }
     request(options, (err, response) => {
-        console.log(chalk`
+        if (response) {
+            if(response.body.error){
+                console.log(chalk.bgRed.yellow(response.body.error.message));
+                return
+            }
+            console.log(chalk`
 {bold.white.bgBlack Country Name:} {green ${response.body.location.country}}`)
-        console.log(chalk`
+            console.log(chalk`
 {bold.white.bgBlack Region:} {yellow ${response.body.location.region}}`)
-        console.log(chalk`
+            console.log(chalk`
         {underline.magenta.italic Weather}
         `)
-        console.log(chalk`
+            console.log(chalk`
 {bold.white.bgBlack Temperature In Celisus:} {blueBright.bold ${response.body.current.temp_c}} {red C} `)
-        if (response && response.body.location.country) {
-            const newAPIKey = "89b7784face7425d8fd5a69979a3bbfa"
-            const newsLink = `https://newsapi.org/v2/top-headlines?q=${response.body.location.country}&
+            if (response.body.location.country) {
+                const newAPIKey = "89b7784face7425d8fd5a69979a3bbfa"
+                const newsLink = `https://newsapi.org/v2/top-headlines?q=${response.body.location.country}&
             sources=bbc-news&apiKey=${newAPIKey}`
-            const optionsForNews = {
-                url: newsLink,
-                method: "GET",
-                json: true,
-            }
-            request(optionsForNews, (err, res) => {
-                if (res && res.body.status === "ok") {
-                    if (res.body.totalResults !== 0) {
-                        console.log(chalk`
+                const optionsForNews = {
+                    url: newsLink,
+                    method: "GET",
+                    json: true,
+                }
+                request(optionsForNews, (err, res) => {
+                    if (res && res.body.status === "ok") {
+                        if (res.body.totalResults !== 0) {
+                            console.log(chalk`
 
 
                             {bold.redBright.underline Top News of ${response.body.location.country}:} 
                             `)
-                        res.body.articles.map((v, i) => {
+                            res.body.articles.map((v, i) => {
 
-                            console.log(chalk`
-{white ${i+1}.} {bold.magentaBright Title}: 
+                                console.log(chalk`
+{white ${i + 1}.} {bold.magentaBright Title}: 
         `+ v.title);
-                            console.log(chalk`
+                                console.log(chalk`
 {bold.yellow Source: }
         `+ v.source.name);
-                            console.log(chalk`
+                                console.log(chalk`
 {bold.blue Discription: } 
             ${v.description}
             
             
             `)
-                        })
+                            })
+                        }
+                        else {
+                            console.log(chalk.bold.bgYellow.white("Sorry, No Top News"))
+                        }
                     }
                     else {
-                        console.log(chalk.bold.bgYellow.white("Sorry, No Top News"))
+                        console.log(chalk.bold.red("Sorry, something went wrong!"))
                     }
-                }
-                else {
-                    console.log(chalk.bold.red("Sorry, something went wrong!"))
-                }
-            })
+                })
+            }
+            else{
+                console.log(chalk.bold.red("Sorry, something went wrong!"))
+            }
         }
         else {
-            cconsole.log(chalk.bold.red("Sorry, something went wrong!"))
+        console.log(chalk.bold.red("Sorry, something went wrong!"))
         }
     })
 }
